e0730a4e37cb3b5bbbb06f70c316fa613a2312bb
Brandon Ly <brandonly@lostcoding.com>
2025-01-11 16:25:15 -0600

    Use query params

--- packages/server/src/routes/log-routes.ts
+++ packages/server/src/routes/log-routes.ts
@@ -1,13 +1,18 @@
 import type { FastifyInstance } from "fastify";
 import { getLogs } from "../utils/logs";
 
+interface LogQueryParams {
+	file: string;
+}
+
 export async function logRoutes(
 	fastify: FastifyInstance,
-	options: Record<string, unknown>,
+	_options: Record<string, unknown>,
 ) {
-	fastify.get("/logs", async (_, response) => {
-		const logs = await getLogs();
+	fastify.get("/logs", async (req, resp) => {
+		const { file } = req.query as LogQueryParams;
+		const logs = await getLogs(file);
 
-		response.send(logs);
+		resp.send(logs);
 	});
 }
--- packages/server/src/utils/logs.ts
+++ packages/server/src/utils/logs.ts
@@ -1,12 +1,15 @@
 import { open } from "node:fs/promises";
 import { DATE_REGEX } from "./regexes";
 
-export async function getLogs() {
-	const testFilePath = "/var/logs/usermanagerd.log.0";
+export async function getLogs(fileName: string) {
+	const file = await open(`/var/logs/${fileName}`);
 
 	const fileContentArr = [];
-	const file = await open(testFilePath);
 
+	// Read file line by line to save on memory.
+	// Instead of reading the whole file, then splitting that into another
+	// array, we can look at each chunk and reverse each chunk by the
+	//
 	for await (const chunk of file.createReadStream()) {
 		const chunkBuf = chunk as Buffer;
 
