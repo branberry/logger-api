43438ed69f154a396b721318dda1e22bcdf4a028
Brandon Ly <brandonly@lostcoding.com>
2025-01-15 18:58:42 -0600

    Remove test code from index and add comments

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -19,28 +19,3 @@ fastify.listen({ port: 3000 }, (err, address) => {
 	}
 	console.log(`Server is now listening on ${address}`);
 });
-
-const testString = `ver: fetchPersona entitlement failure:88737
-2024-12-28 15:19:02.050319Z [82286] (0x16b86f000) In UMSyncServer: fetchPersona entitlement failure:89072
-2024-12-28 15:23:45.232597Z [82286] (0x16b8fb000) In UMSyncServer: fetchPersona entitlement failure:89129
-2024-12-28 15:29:39.877980Z [82286] (0x16b8fb000) In UMSyncServer: fetchPersona entitlement failure:89291
-2024-12-28 17:33:43.139496Z [82286] (0x16b8fb000) In UMSyncServer: fetchPersona entitlement failure:92741
-2024-12-28 18:55:11.349288Z [94385] (0x203f80240) Loaded Persona Generation ID from manifest:1
-2024-12-28 18:55:11.359552Z [94385] (0x203f80240) Adding 0 entry to persona Table for user:FFFFEEEE-DDDD-CCCC-BBBB-AAAA000000F8
-2024-12-28 18:55:11.359657Z [94385] (0x203f80240) Adding 199 entry to persona Table for user:FFFFEEEE-DDDD-CCCC-BBBB-AAAA000000F8
-2024-12-28 18:55:11.359693Z [94385] (0x203f80240) Adding 99 entry to persona Table for user:FFFFEEEE-DDDD-CCCC-BBBB-AAAA000000F8
-2024-12-28 18:55:11.359732Z [94385] (0x203f80240) Adding 1000 entry to persona Table for user:FFFFEEEE-DDDD-CCCC-BBBB-AAAA000000F8
-2024-12-28 18:55:11.359781Z [94385] (0x203f80240) Adding 1001 entry to persona Table for user:7A6F7608-148A-4216-82C0-6B055EB52044`;
-
-const testArr = testString.split(DATE_REGEX);
-
-if (testArr.length % 2 === 1) {
-	const remainder = testArr[0];
-	// this is the logic to capture remainders
-	const validEntries = testArr.slice(1);
-	const result = [];
-	for (let i = 0; i < validEntries.length - 1; i += 2) {
-		result.push(`${validEntries[i]}${validEntries[i + 1]}`);
-	}
-	console.log(result.reverse().join(""));
-}
--- packages/server/src/utils/logs.ts
+++ packages/server/src/utils/logs.ts
@@ -14,24 +14,38 @@ const BUF_SIZE = 2 ** 16;
  * An async generator function that efficiently reads log files in descending order.
  * The file is read using node's `read` function, which takes in a pre-defined buffer as input.
  * It allows for us to read the file in reverse by specifying the position in the file we want to begin with.
- * @param fileName The path to the log file in `/var/logs/
+ * @param fileName The path to the log file in `/var/logs/`
  *
  */
 export async function* getLogs(fileName: string) {
 	const file = await open(fileName);
+
+	// Retrieving the size of the file in bytes. This will
+	// help us determine where we need to start in the file
+	// so that we can display the logs in descending order (i.e. the latest logs first)
 	const { size } = await file.stat();
 
 	let totalBytesRead = 0;
 	let remainder = "";
+
 	while (totalBytesRead < size) {
-		const buffer = Buffer.alloc(Math.min(size - totalBytesRead, BUF_SIZE));
+		// Doing math.min here because as we get near the end of the file (or, the beginning, in this case)
+		// we'll probably have the last buffer be somewhat smaller than the standard chunk size.
+		// This is to ensure we don't read duplicate data.
+		const bufferSize = Math.min(size - totalBytesRead, BUF_SIZE);
+
+		const buffer = Buffer.alloc(bufferSize);
+
+		// This is where we go backwards. To calculate our current position at each iteration,
+		//
+		const currPosition = Math.max(0, size - BUF_SIZE - totalBytesRead);
 
 		const { bytesRead } = await readAsync(
 			file.fd,
 			buffer,
 			0,
-			Math.min(size - totalBytesRead, BUF_SIZE),
-			Math.max(0, size - BUF_SIZE - totalBytesRead),
+			bufferSize,
+			currPosition,
 		);
 
 		totalBytesRead += bytesRead;
@@ -51,11 +65,8 @@ export async function* getLogs(fileName: string) {
 		}
 
 		for (let i = logEntries.length - 1; i >= 0; i -= 2) {
-			console.log(`${logEntries[i - 1]}${logEntries[i]}`);
 			yield `${logEntries[i - 1]}${logEntries[i]}`;
 		}
-		// yield buffer.toString().split(DATE_REGEX).reverse().join("");
 	}
-	console.log(totalBytesRead, size);
 	await file.close();
 }
